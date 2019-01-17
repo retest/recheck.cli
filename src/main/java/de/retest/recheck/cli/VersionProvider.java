b08a1d3af89554d276fad028b56e8f971fc37edf
Marius Kemmling <marius.kemmling@retest.de>
2019-01-17 11:20:56 +0100

    Implement recheck version and java version

--- src/main/java/de/retest/recheck/cli/VersionProvider.java
+++ src/main/java/de/retest/recheck/cli/VersionProvider.java
@@ -5,8 +5,8 @@ import picocli.CommandLine.IVersionProvider;
 public class VersionProvider implements IVersionProvider {
 
 	@Override
-	public String[] getVersion() throws Exception {
-		return new String[] { "recheck CLI version " + getClass().getPackage().getImplementationVersion() };
+	public String[] getVersion() throws RuntimeException {
+		return new String[] { "recheck CLI version " + this.getClass().getPackage().getImplementationVersion(),
+				"recheck Java version " +  Runtime.class.getPackage().getImplementationVersion() };
 	}
-
 }

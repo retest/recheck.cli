98227e6ab878e02cabe00269848c969a3eaaa7d4
Johannes Dillmann <johannes.dillmann@retest.de>
2019-01-21 18:06:37 +0100

    Use proper release layout

--- src/main/package/bin/recheck.cmd
+++ src/main/package/bin/recheck.cmd
@@ -1,11 +1,11 @@
 @echo off
 
 :: recheck-cli installation directory.
-SET RETEST_HOME=%~dp0
+SET RECHECK_HOME=%~dp0\..
 
 SET JAVA=java
 
 SET JAVA_ARGS=-XX:+HeapDumpOnOutOfMemoryError
 SET JAVA_ARGS=%JAVA_ARGS% -XX:-OmitStackTraceInFastThrow
 
-%JAVA% %JAVA_ARGS% -jar "%RETEST_HOME%\recheck-cli.jar" %* 2>&1
+%JAVA% %JAVA_ARGS% -jar "%RECHECK_HOME%\lib\recheck-cli.jar" %* 2>&1

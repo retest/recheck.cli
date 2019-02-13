b4e2acaefaa2ab58e9901645f0bec05a1d9b359e
Enes Özel <enes.oezel@retest.de>
2019-02-13 16:45:13 +0100

    Add log message

--- src/main/java/de/retest/recheck/cli/ReplayResultLoader.java
+++ src/main/java/de/retest/recheck/cli/ReplayResultLoader.java
@@ -3,6 +3,9 @@ package de.retest.recheck.cli;
 import java.io.File;
 import java.io.IOException;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import de.retest.recheck.LoadRecheckIgnoreUtil;
 import de.retest.recheck.configuration.Configuration;
 import de.retest.recheck.persistence.Persistence;
@@ -11,10 +14,13 @@ import de.retest.recheck.report.ReplayResult;
 
 public class ReplayResultLoader {
 
+	private static final Logger logger = LoggerFactory.getLogger( ReplayResultLoader.class );
+
 	private ReplayResultLoader() {}
 
 	public static ReplayResult load( final File replayResult ) throws IOException {
 		Configuration.ensureLoaded();
+		logger.info( "Checking test report in path '{}'.", replayResult.getPath() );
 		LoadRecheckIgnoreUtil.loadRecheckIgnore();
 		final Persistence<ReplayResult> resultPersistence = new KryoPersistence<>();
 		return resultPersistence.load( replayResult.toURI() );

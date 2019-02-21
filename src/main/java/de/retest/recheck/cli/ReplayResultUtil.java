de0716fcb0842fd005928a56ffd8361f8485c5d3
Marius Kemmling <marius.kemmling@retest.de>
2019-02-21 15:53:07 +0100

    Remove no longer available class

--- src/main/java/de/retest/recheck/cli/ReplayResultUtil.java
+++ src/main/java/de/retest/recheck/cli/ReplayResultUtil.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.retest.recheck.configuration.Configuration;
 import de.retest.recheck.persistence.Persistence;
 import de.retest.recheck.persistence.bin.KryoPersistence;
 import de.retest.recheck.report.ReplayResult;
@@ -18,7 +17,6 @@ public class ReplayResultUtil {
 	private ReplayResultUtil() {}
 
 	public static ReplayResult load( final File replayResult ) throws IOException {
-		Configuration.ensureLoaded();
 		logger.info( "Checking test report in path '{}'.", replayResult.getPath() );
 		final Persistence<ReplayResult> resultPersistence = new KryoPersistence<>();
 		return resultPersistence.load( replayResult.toURI() );

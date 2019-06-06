c280763453b6535d72a114f34353cf9f16f43a96
IvanKaradzhov <ivan.karadzhov@retest.de>
2019-06-06 16:32:55 +0200

    create TestReportFilterUtil.java class

--- src/main/java/de/retest/recheck/cli/TestReportFilterUtil.java
+++ src/main/java/de/retest/recheck/cli/TestReportFilterUtil.java
@@ -5,6 +5,7 @@ import static de.retest.recheck.ignore.RecheckIgnoreUtil.loadRecheckIgnore;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import de.retest.recheck.ignore.CompoundFilter;
@@ -18,11 +19,13 @@ public class TestReportFilterUtil {
 			return Filter.FILTER_NOTHING;
 		}
 		final List<Filter> filters = new ArrayList<>();
-		final Set<String> filterKeys = SearchFilterFiles.toFileNameFilterMapping().keySet();
+		final Map<String, Filter> fileNameFilterMapping = SearchFilterFiles.toFileNameFilterMapping();
+		final Set<String> filterKeys = fileNameFilterMapping.keySet();
+
 		for ( final String filterName : exclude ) {
 			for ( final String key : filterKeys ) {
 				if ( filterName.equals( key ) ) {
-					filters.add( SearchFilterFiles.toFileNameFilterMapping().get( key ) );
+					filters.add( fileNameFilterMapping.get( key ) );
 				}
 			}
 		}

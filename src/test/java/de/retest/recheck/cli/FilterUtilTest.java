f5874ac80f37accfd6973eb2de16c1e7f96d8fc5
IvanKaradzhov <ivan.karadzhov@retest.de>
2019-06-06 18:04:18 +0200

    Rename the test and adapt the changes

--- src/test/java/de/retest/recheck/cli/FilterUtilTest.java
+++ src/test/java/de/retest/recheck/cli/FilterUtilTest.java
@@ -12,29 +12,29 @@ import org.junit.jupiter.api.Test;
 import de.retest.recheck.ignore.CompoundFilter;
 import de.retest.recheck.ignore.Filter;
 
-class TestReportFilterUtilTest {
+class FilterUtilTest {
 
 	@Test
-	void when_exclude_is_not_null_the_size_plus_one_should_be_returned() throws IOException {
+	void when_exclude_is_not_empty_the_size_plus_GlobalIgnoreApplier_should_be_returned() throws IOException {
 		// +1 for GlobalIgnoreApplier.
 		final List<String> exclude = Arrays.asList( "positioning.filter" );
-		final CompoundFilter filter = (CompoundFilter) TestReportFilterUtil.checkAndCollectFilterNames( exclude );
+		final CompoundFilter filter = (CompoundFilter) FilterUtil.getExcludeFilterFiles( exclude );
 		// +1 for GlobalIgnoreApplier.
 		assertThat( filter.getFilters() ).hasSize( exclude.size() + 1 );
 	}
 
 	@Test
-	void when_exclude_is_null_one_should_be_returned() throws IOException {
+	void when_exclude_is_empty_only_GlobalIgnoreApplier_should_be_returned() throws IOException {
 		// +1 for GlobalIgnoreApplier.
 		final List<String> exclude = new ArrayList<>();
-		final CompoundFilter filter = (CompoundFilter) TestReportFilterUtil.checkAndCollectFilterNames( exclude );
+		final CompoundFilter filter = (CompoundFilter) FilterUtil.getExcludeFilterFiles( exclude );
 		// +1 for GlobalIgnoreApplier.
 		assertThat( filter.getFilters() ).hasSize( exclude.size() + 1 );
 	}
 
 	@Test
-	void when_exclude_is_null_filter_nothing_should_be_returned() throws Exception {
-		final Filter filter = TestReportFilterUtil.checkAndCollectFilterNames( null );
+	void when_exclude_is_empty_filter_nothing_should_be_returned() throws Exception {
+		final Filter filter = FilterUtil.getExcludeFilterFiles( null );
 		assertThat( filter ).isSameAs( Filter.FILTER_NOTHING );
 	}
 

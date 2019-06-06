df5ba5ef7e576cfa91ede9638d6dfd610fa23d59
IvanKaradzhov <ivan.karadzhov@retest.de>
2019-06-06 16:58:58 +0200

    create test for TestReportFilterUtil class

---
+++ src/test/java/de/retest/recheck/cli/TestReportFilterUtilTest.java
@@ -0,0 +1,41 @@
+package de.retest.recheck.cli;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import de.retest.recheck.ignore.CompoundFilter;
+import de.retest.recheck.ignore.Filter;
+
+class TestReportFilterUtilTest {
+
+	@Test
+	void attributeDifference_should_be_filtered_after_using_checkAndCollectFilterNames() throws IOException {
+		// +1 for GlobalIgnoreApplier.
+		final List<String> exclude = Arrays.asList( "positioning.filter" );
+		final CompoundFilter filter = (CompoundFilter) TestReportFilterUtil.checkAndCollectFilterNames( exclude );
+		// +1 for GlobalIgnoreApplier.
+		assertThat( filter.getFilters() ).hasSize( exclude.size() + 1 );
+	}
+
+	@Test
+	void attributeDifference_should_not_be_filtered_using_checkAndCollectFilterNames() throws IOException {
+		// +1 for GlobalIgnoreApplier.
+		final List<String> exclude = new ArrayList<>();
+		final CompoundFilter filter = (CompoundFilter) TestReportFilterUtil.checkAndCollectFilterNames( exclude );
+		// +1 for GlobalIgnoreApplier.
+		assertThat( filter.getFilters() ).hasSize( exclude.size() + 1 );
+	}
+
+	@Test
+	void when_exclude_is_null_filter_nothing_should_be_returned() throws Exception {
+		final Filter filter = TestReportFilterUtil.checkAndCollectFilterNames( null );
+		assertThat( filter ).isSameAs( Filter.FILTER_NOTHING );
+	}
+
+}

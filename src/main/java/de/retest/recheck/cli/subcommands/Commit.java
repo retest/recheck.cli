8692b99f6e4870c2e4c60a346bfdae87e92e637f
IvanKaradzhov <ivan.karadzhov@retest.de>
2019-06-12 10:43:13 +0200

    catch KryoException in Commit class

--- src/main/java/de/retest/recheck/cli/subcommands/Commit.java
+++ src/main/java/de/retest/recheck/cli/subcommands/Commit.java
@@ -8,6 +8,8 @@ import java.util.HashSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.esotericsoftware.kryo.KryoException;
+
 import de.retest.recheck.cli.PreCondition;
 import de.retest.recheck.cli.RecheckCli;
 import de.retest.recheck.cli.TestReportUtil;
@@ -60,6 +62,9 @@ public class Commit implements Runnable {
 			}
 		} catch ( final IOException e ) {
 			logger.error( "An error occurred while loading the test report!", e );
+		} catch ( final KryoException e ) {
+			logger.error( "The report was created with another, incompatible recheck version.\r\n"
+					+ "Please, use the same recheck version to load a report with which it was generated." );
 		}
 	}
 

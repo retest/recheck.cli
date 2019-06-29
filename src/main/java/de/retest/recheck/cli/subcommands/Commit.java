5e6393cc6490c9188085d16ff8e788e388d370d9
IvanKaradzhov <ivan.karadzhov@retest.de>
2019-06-29 12:37:53 +0200

    Use the exclude filter for the commit command

--- src/main/java/de/retest/recheck/cli/subcommands/Commit.java
+++ src/main/java/de/retest/recheck/cli/subcommands/Commit.java
@@ -4,20 +4,24 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.esotericsoftware.kryo.KryoException;
 
+import de.retest.recheck.cli.FilterUtil;
 import de.retest.recheck.cli.PreCondition;
 import de.retest.recheck.cli.RecheckCli;
 import de.retest.recheck.cli.TestReportUtil;
+import de.retest.recheck.ignore.Filter;
 import de.retest.recheck.persistence.NoGoldenMasterFoundException;
 import de.retest.recheck.persistence.Persistence;
 import de.retest.recheck.persistence.PersistenceFactory;
 import de.retest.recheck.persistence.xml.util.StdXmlClassesProvider;
 import de.retest.recheck.report.TestReport;
+import de.retest.recheck.report.TestReportFilter;
 import de.retest.recheck.suite.flow.ApplyChangesToStatesFlow;
 import de.retest.recheck.suite.flow.CreateChangesetForAllDifferencesFlow;
 import de.retest.recheck.ui.descriptors.SutState;
@@ -38,6 +42,9 @@ public class Commit implements Runnable {
 	@Option( names = "--all", description = "Accept all differences from the given test report." )
 	private boolean all;
 
+	@Option( names = "--exclude", description = "Filter(s) to exclude changes from the diff." )
+	private List<String> exclude;
+
 	@Parameters( arity = "1", description = RecheckCli.REPORT_FILE_PARAM_DESCRIPTION )
 	private File testReport;
 
@@ -51,12 +58,14 @@ public class Commit implements Runnable {
 		}
 		try {
 			final TestReport report = TestReportUtil.load( testReport );
-			if ( !report.containsChanges() ) {
+			final Filter excludeFilter = FilterUtil.getExcludeFilterFiles( exclude );
+			final TestReport filteredTestReport = TestReportFilter.filter( report, excludeFilter );
+			if ( !filteredTestReport.containsChanges() ) {
 				logger.warn( "The test report has no differences." );
 				return;
 			}
-			TestReportUtil.print( report, testReport );
-			final ReviewResult reviewResult = CreateChangesetForAllDifferencesFlow.create( report );
+			TestReportUtil.print( filteredTestReport, testReport );
+			final ReviewResult reviewResult = CreateChangesetForAllDifferencesFlow.create( filteredTestReport );
 			for ( final SuiteChangeSet suiteChangeSet : reviewResult.getSuiteChangeSets() ) {
 				applyChanges( createSutStatePersistence(), suiteChangeSet );
 			}

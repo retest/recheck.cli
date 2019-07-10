4c2655870e27f9a737b4c39c975884ba9697bf57
Daniel Kraus <daniel.kraus@retest.de>
2019-07-10 11:08:26 +0200

    Create exception type for wrong test report format

---
+++ src/main/java/de/retest/recheck/cli/TestReportFormatException.java
@@ -0,0 +1,9 @@
+package de.retest.recheck.cli;
+
+import java.io.IOException;
+
+public class TestReportFormatException extends IOException {
+
+	private static final long serialVersionUID = 1L;
+
+}

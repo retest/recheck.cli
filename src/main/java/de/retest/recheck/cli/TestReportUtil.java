0d74e85b2c53874abc326c49ed2473b46346f5ba
Daniel Kraus <daniel.kraus@retest.de>
2019-07-10 11:09:08 +0200

    Add check for extension when loading test reports

--- src/main/java/de/retest/recheck/cli/TestReportUtil.java
+++ src/main/java/de/retest/recheck/cli/TestReportUtil.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.retest.recheck.Properties;
 import de.retest.recheck.persistence.Persistence;
 import de.retest.recheck.persistence.bin.KryoPersistence;
 import de.retest.recheck.report.TestReport;
@@ -16,10 +17,17 @@ public class TestReportUtil {
 
 	private TestReportUtil() {}
 
-	public static TestReport load( final File testReportFile ) throws IOException {
+	public static TestReport load( final File testReportFile ) throws TestReportFormatException, IOException {
 		logger.info( "Checking test report in path '{}'.", testReportFile.getPath() );
+		checkExtension( testReportFile );
 		final Persistence<TestReport> persistence = new KryoPersistence<>();
 		return persistence.load( testReportFile.toURI() );
+	}
+
+	private static void checkExtension( final File testReportFile ) throws TestReportFormatException {
+		if ( !testReportFile.getName().endsWith( Properties.TEST_REPORT_FILE_EXTENSION ) ) {
+			throw new TestReportFormatException();
+		}
 	}
 
 	public static void print( final TestReport testReport, final File testReportFile ) {

8863055a0bd45b68fa574f5fa29ae1ddab6cd2db
Daniel Kraus <daniel.kraus@retest.de>
2019-08-20 16:24:53 +0200

    Fix formatting

--- src/main/java/de/retest/recheck/cli/WarningUtil.java
+++ src/main/java/de/retest/recheck/cli/WarningUtil.java
@@ -14,8 +14,7 @@ public class WarningUtil {
 
 	private static final Logger logger = LoggerFactory.getLogger( WarningUtil.class );
 
-	private WarningUtil() {
-	}
+	private WarningUtil() {}
 
 	public static void logWarnings( final ReviewResult reviewResult ) {
 		reviewResult.getAllAttributeDifferences().stream() //
@@ -27,21 +26,27 @@ public class WarningUtil {
 	private static Function<AttributeDifference, String> toWarningMessage() {
 		return attributeDifference -> {
 			final ElementIdentificationWarning warning = attributeDifference.getElementIdentificationWarning();
+
 			if ( warning == null ) {
 				return null;
 			}
+
 			final String title = "*************** recheck warning ***************\n";
+
 			final String elementIdentifier = attributeDifference.getKey();
 			final String expectedValue = attributeDifference.getExpectedToString();
 			final String actualValue = attributeDifference.getActualToString();
 			final String elementIdentifierInfo =
 					String.format( "The HTML %s attribute used for element identification changed from %s to %s.\n",
 							elementIdentifier, expectedValue, actualValue );
+
 			final String info = "recheck identified the element based on the persisted Golden Master.\n";
+
 			final String testClassName = warning.getTestClassName();
 			final String onApplyChangesInfo = //
 					String.format( "If you apply these changes to the Golden Master, your test %s will break.",
 							testClassName );
+
 			return title + elementIdentifierInfo + info + onApplyChangesInfo;
 		};
 	}

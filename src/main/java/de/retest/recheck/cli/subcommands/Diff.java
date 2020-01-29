df03a26b208a3da6adfb53205b8898f82273a988
lemoncurry <agape.anna@gmail.com>
2020-01-29 13:50:39 +0100

    Change 'diff' to compare two Golden Masters
    Diff will now compare two Golden Masters, the results may be preserved as test report. The previous command 'diff' that displayed results of a test report may be used with 'diff' for a transitional period, with a warning.

--- src/main/java/de/retest/recheck/cli/subcommands/Diff.java
+++ src/main/java/de/retest/recheck/cli/subcommands/Diff.java
@@ -1,73 +1,164 @@
 package de.retest.recheck.cli.subcommands;
 
-import static de.retest.recheck.printer.DefaultValueFinderProvider.none;
+import static de.retest.recheck.XmlTransformerUtil.getXmlTransformer;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import de.retest.recheck.cli.PreCondition;
-import de.retest.recheck.cli.TestReportFormatException;
+import de.retest.recheck.RecheckProperties;
+import de.retest.recheck.SuiteAggregator;
 import de.retest.recheck.cli.utils.ErrorHandler;
 import de.retest.recheck.cli.utils.FilterUtil;
-import de.retest.recheck.cli.utils.TestReportUtil;
-import de.retest.recheck.ignore.CompoundFilter;
+import de.retest.recheck.cli.utils.GoldenMasterUtil;
+import de.retest.recheck.execution.RecheckDifferenceFinder;
 import de.retest.recheck.ignore.Filter;
-import de.retest.recheck.printer.TestReportPrinter;
+import de.retest.recheck.persistence.GoldenMasterProvider;
+import de.retest.recheck.persistence.GoldenMasterProviderImpl;
+import de.retest.recheck.persistence.PersistenceFactory;
+import de.retest.recheck.printer.ActionReplayResultPrinter;
+import de.retest.recheck.report.ActionReplayResult;
+import de.retest.recheck.report.SuiteReplayResult;
+import de.retest.recheck.report.TestReplayResult;
 import de.retest.recheck.report.TestReport;
 import de.retest.recheck.report.TestReportFilter;
+import de.retest.recheck.ui.descriptors.SutState;
+import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-@Command( name = "diff", description = "Display differences of given test report." )
+@Command( name = "diff", description = "Compare two Golden Masters." )
 public class Diff implements Runnable {
 
 	private static final Logger logger = LoggerFactory.getLogger( Diff.class );
 
-	@Option( names = "--help", usageHelp = true, hidden = true )
-	private boolean displayHelp;
+	private static final PersistenceFactory persistenceFactory = new PersistenceFactory( getXmlTransformer() );
+	private static final GoldenMasterProvider goldenMasterProvider =
+			new GoldenMasterProviderImpl( persistenceFactory.getPersistence() );
+
+	private static final String TEST_NAME = "Test - Golden Master Comparison";
+	private static final String SUITE_NAME = "Suite - Golden Master Comparison";
+
+	// TODO Temporarily it will be allowed to use the diff command for display of test reports
+	// TODO Will be removed in following version (see RET-1956); instead, use the command "show"
+	@Parameters( arity = "1..2", description = GoldenMasterUtil.GOLDEN_MASTER_PARAMETER_DESCRIPTION )
+	private Path[] goldenMasterPath;
 
 	@Option( names = "--exclude", description = FilterUtil.EXCLUDE_OPTION_DESCRIPTION )
 	private List<String> exclude;
 
-	@Parameters( arity = "1", description = TestReportUtil.TEST_REPORT_PARAMETER_DESCRIPTION )
-	private File testReport;
+	@Option( names = "--help", usageHelp = true, hidden = true )
+	private boolean displayHelp;
+
+	@Option( names = "--output", arity = "1",
+			description = "Save differences of Golden Masters to specified directory as test report." )
+	private Path directory;
 
 	@Override
 	public void run() {
-		if ( !PreCondition.isSatisfied() ) {
-			return;
-		}
-		try {
-			final List<String> invalidFilters = FilterUtil.getInvalidFilters( exclude );
-			if ( !invalidFilters.isEmpty() ) {
-				FilterUtil.logWarningForInvalidFilters( invalidFilters );
-			} else {
-				printDiff();
+		// TODO: until the following version (see RET-1956), a user may still use the diff command to print test report differences.
+		// TODO: remove in next version (see RET-1956)
+		if ( goldenMasterPath.length == 1 ) {
+			logger.warn(
+					"The diff command for viewing a test report is deprecated and will be removed in the following version."
+							+ " Please use the new command 'show' to display a test report." );
+
+			final List<String> argsAsList = new ArrayList<>();
+			argsAsList.add( goldenMasterPath[0].toString() );
+			if ( exclude != null ) {
+				for ( final String filter : exclude ) {
+					argsAsList.add( "--exclude" );
+					argsAsList.add( filter );
+				}
 			}
-		} catch ( final Exception e ) {
-			ErrorHandler.handle( e );
+
+			final String[] args = argsAsList.toArray( new String[0] );
+			new CommandLine( new Show() ).execute( args );
+
+		} else {
+
+			final Path expectedSutStatePath = goldenMasterPath[0];
+			final Path actualSutStatePath = goldenMasterPath[1];
+
+			try {
+				if ( !FilterUtil.hasValidExcludeOption( exclude ) ) {
+					return;
+				}
+				final ActionReplayResult actionReplayResult =
+						createActionReplayResultFrom( expectedSutStatePath, actualSutStatePath );
+				printGoldenMasterDifferences( filterActionReplayResult( actionReplayResult ) );
+				persistIfOutputOptionPresent( actionReplayResult );
+			} catch ( final Exception e ) {
+				ErrorHandler.handle( e );
+			}
 		}
 	}
 
-	private void printDiff() throws TestReportFormatException, IOException {
-		final TestReport report = TestReportUtil.load( testReport );
-		final Filter excludeFilter = FilterUtil.getExcludeFilterFiles( exclude );
-		final Filter recheckIgnore = FilterUtil.loadRecheckIgnore();
-		final TestReportFilter filter = new TestReportFilter( new CompoundFilter( excludeFilter, recheckIgnore ) );
-		final TestReport filteredTestReport = filter.filter( report );
-		final TestReportPrinter printer = new TestReportPrinter( none() );
+	private ActionReplayResult createActionReplayResultFrom( final Path expectedSutStatePath,
+			final Path actualSutStatePath ) {
+		final SutState expectedSutState = loadGoldenMaster( expectedSutStatePath );
+		final SutState actualSutState = loadGoldenMaster( actualSutStatePath );
 
-		logger.info( "\n{}", printer.toString( filteredTestReport ) );
+		return findDifferences( expectedSutStatePath.toString(), expectedSutState, actualSutState );
+	}
 
-		if ( filteredTestReport.getDifferencesCount() > 0 ) {
-			logger.info( "\nOverall, recheck found {} difference(s) when checking {} element(s).",
-					filteredTestReport.getDifferencesCount(), filteredTestReport.getCheckedUiElementsCount() );
+	private SutState loadGoldenMaster( final Path actualSutStatePath ) {
+		return goldenMasterProvider
+				.loadGoldenMaster( actualSutStatePath.resolve( RecheckProperties.DEFAULT_XML_FILE_NAME ).toFile() );
+	}
+
+	private ActionReplayResult findDifferences( final String expectedSutStatePath, final SutState expectedSutState,
+			final SutState actualSutState ) {
+		final RecheckDifferenceFinder differenceFinder = new RecheckDifferenceFinder(
+				( attributes, s, serializable ) -> false, "Comparison of Golden Masters", expectedSutStatePath );
+		return differenceFinder.findDifferences( expectedSutState, actualSutState );
+	}
+
+	private void printGoldenMasterDifferences( final ActionReplayResult actionReplayResult ) {
+		final ActionReplayResultPrinter printer =
+				new ActionReplayResultPrinter( ( attributes, s, serializable ) -> false );
+
+		logger.info( "\n{}", printer.toString( actionReplayResult ) );
+
+		final int differenceCount = actionReplayResult.getDifferences().size();
+		if ( differenceCount > 0 ) {
+			logger.info( "\nOverall, recheck found {} difference(s) when checking {} element(s).", differenceCount,
+					actionReplayResult.getCheckedUiElementsCount() );
 		}
+	}
+
+	private ActionReplayResult filterActionReplayResult( final ActionReplayResult actionReplayResult ) {
+		final Filter excludeFilter = FilterUtil.getExcludeFilterFiles( exclude );
+		final TestReportFilter reportFilter = new TestReportFilter( (excludeFilter) );
+		return reportFilter.filter( actionReplayResult );
+	}
+
+	private void persistIfOutputOptionPresent( final ActionReplayResult actionReplayResult ) throws IOException {
+		if ( directory != null ) {
+			persist( actionReplayResult, directory );
+			return;
+		}
+		logger.info( "\nThe displayed differences may be saved as test report by using the --output option." );
+	}
+
+	private void persist( final ActionReplayResult result, final Path outputDirectory ) throws IOException {
+		final TestReplayResult testPlayResult = new TestReplayResult( TEST_NAME, 1 );
+		final SuiteReplayResult suiteReplayResult = SuiteAggregator.getInstance().getSuite( SUITE_NAME );
+		testPlayResult.addAction( result );
+		suiteReplayResult.addTest( testPlayResult );
+		final TestReport testReport = new TestReport( suiteReplayResult );
+
+		final File report = outputDirectory.resolve( RecheckProperties.AGGREGATED_TEST_REPORT_FILE_NAME ).toFile();
+
+		persistenceFactory.getPersistence().save( report.toURI(), testReport );
+
+		logger.info( "\nThe test report has been saved to '{}'.", report.getAbsolutePath() );
 	}
 
 }
